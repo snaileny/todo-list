94b5213e9d125fa31db6f767f5fa93de35f07015
snaileny <emirerek@hotmail.com.tr>
2022-09-07 18:58:21 +0300

    Seperate UI code as a module

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 import App from "./modules/app.js";
+import UI from "./modules/ui.js";
 import "./css/index.css";
-
-const inputs = {
-
-    filterActive: document.querySelector("#task-active-filter"),
-    filterDate: document.querySelector("#task-date-filter"),
-    taskTitle: document.querySelector(".task-title-input"),
-    taskDate: document.querySelector(".task-date-input"),
-    projectName: document.querySelector("#project-name-input")
-
-}
 
 const elements = {
 
     projectList: document.querySelector(".project-list"),
-    taskList: document.querySelector(".task-list")
+    taskList: document.querySelector(".task-list"),
+    inputElements: {
+
+        filterActive: document.querySelector("#task-active-filter"),
+        filterDate: document.querySelector("#task-date-filter"),
+        taskTitle: document.querySelector(".task-title-input"),
+        taskDate: document.querySelector(".task-date-input"),
+        projectName: document.querySelector("#project-name-input")
+    
+    }
 
 }
 
@@ -26,13 +26,14 @@ const errors = {
 }
 
 const app = new App();
-render();
+const ui = new UI(app, elements)
+ui.render();
 
 function eventHandler(e) {
 
-    const taskTitle = inputs.taskTitle.value;
-    const taskDate = inputs.taskDate.value;
-    const projectName = inputs.projectName.value;
+    const taskTitle = elements.inputElements.taskTitle.value;
+    const taskDate = elements.inputElements.taskDate.value;
+    const projectName = elements.inputElements.projectName.value;
 
     if (e.target.className === "add-task-button") {
 
@@ -44,19 +45,19 @@ function eventHandler(e) {
 
         }
 
-        render();
+        ui.render();
 
     } if (e.target.className === "remove-task-button") {
 
         const taskTitle = e.target.parentElement.dataset.taskTitle;
         app.removeTask(taskTitle, app.getActiveProject());
-        render();
+        ui.render();
 
     } if (e.target.className === "complete-task-checkbox") {
 
         const taskTitle = e.target.parentElement.dataset.taskTitle;
         app.toggleTask(taskTitle, app.getActiveProject());
-        render();
+        ui.render();
 
     } if (e.target.className === "add-project-button") {
 
@@ -69,20 +70,20 @@ function eventHandler(e) {
         }
 
         app.setActiveProject(projectName);
-        render();
+        ui.render();
 
     } if (e.target.className === "project") {
 
         const selectedProject = e.target.dataset.projectName;
         app.setActiveProject(selectedProject);
-        render();
+        ui.render();
 
     } if (e.target.className === "remove-project-button") {
 
         const selectedProject = e.target.parentElement.dataset.projectName;
         app.removeProject(selectedProject);
         app.setActiveProject(app.main.getName());
-        render();
+        ui.render();
 
     } if (e.target.className === "clear-completed-button") {
 
@@ -100,7 +101,7 @@ function eventHandler(e) {
 
         }
 
-        render();
+        ui.render();
 
     } if (e.target.className === "clear-all-button") {
 
@@ -112,147 +113,13 @@ function eventHandler(e) {
 
         }
 
-        render();
+        ui.render();
 
     } if (e.target.tagName === "SELECT") {
 
-        render();
+        ui.render();
 
     }
-
-}
-
-function render() {
-
-    const renderTasks = () => {
-
-        const checkFilterActive = (filterActive, taskActive) => {
-    
-            if (filterActive === "active" && taskActive === true) {
-    
-                return true;
-    
-            } else if (filterActive === "completed" && taskActive === false) {
-    
-                return true;
-    
-            } else if (filterActive === "all") {
-    
-                return true;
-    
-            } else {
-    
-                return false;
-                
-            }
-    
-        }
-    
-        const checkFilterDate = (filterDate, taskTimestamp) => {
-    
-            const CURRENT_DATE = new Date().toISOString().slice(0, 10);
-            const CURRENT_TIMESTAMP = Date.parse(CURRENT_DATE);
-            const TASK_SECONDS = Math.floor(taskTimestamp / 1000);
-            const CURRENT_SECONDS =  Math.floor(CURRENT_TIMESTAMP / 1000);
-            const UPCOMING_SECONDS = 604800;
-    
-            if (TASK_SECONDS === CURRENT_SECONDS && filterDate === "today") {
-    
-                return true;
-    
-            } else if (TASK_SECONDS < CURRENT_SECONDS + UPCOMING_SECONDS && filterDate === "upcoming") {
-    
-                return true;
-    
-            } else if (filterDate === "all") {
-    
-                return true;
-    
-            } else {
-    
-                return false;
-    
-            }
-    
-        }
-        
-        const projects = app.getProjectList();
-        const filterActive = inputs.filterActive.value;
-        const filterDate = inputs.filterDate.value;
-        let taskListTemplate = ``;
-    
-        for (const projectName in projects) {
-    
-            const tasks = projects[projectName].getTaskList();
-    
-            for (const taskTitle in tasks) {
-    
-                const task = tasks[taskTitle];
-                const taskProject = task.getProject();
-                const taskDate = task.getDate();
-                const taskTimestamp = task.getTimestamp();
-                const taskActive = task.isActive();
-                const checkBoxState = taskActive ? "" : "checked";
-                const taskCompleteClass = taskActive ? "" : "-complete";
-        
-                const taskTemplate = `
-                <div class="task${taskCompleteClass}" data-task-title="${taskTitle}" data-task-project="${taskDate}" data-task-active="${taskActive}">
-                    <input type="checkbox" id="complete-${taskTitle}" class="complete-task-checkbox" ${checkBoxState}>
-                    <div class="task-content">
-                        <label for="complete-${taskTitle}" class="task-title">${taskTitle}</label>
-                        <p class="task-date">${taskDate}</p>
-                    </div>
-                    <button type="button" class="remove-task-button">X</button>
-                </div>
-                `;
-        
-                if (app.getActiveProject() === taskProject && checkFilterActive(filterActive, taskActive) && checkFilterDate(filterDate, taskTimestamp)) {
-        
-                    taskListTemplate += taskTemplate;
-        
-                }
-        
-            }
-    
-        }
-    
-        elements.taskList.innerHTML = taskListTemplate;
-    
-    }
-    
-    const renderProjects = () => {
-    
-        const projects = app.getProjectList();
-        let projectListTemplate = ``;
-    
-        for (const projectName in projects) {
-
-            const projectActiveClass = app.getActiveProject() === projectName ? "-active" : "";
-    
-            if (projectName !== app.main.getName()) {
-    
-                const projectTemplate = `
-                <span class="project${projectActiveClass}" data-project-name="${projectName}">${projectName}<button class="remove-project-button">X</button></span>
-                `
-                projectListTemplate += projectTemplate;
-    
-            } else {
-    
-                const projectTemplate = `
-                <span class="project${projectActiveClass}" data-project-name="main">Main</span>
-                `
-                projectListTemplate += projectTemplate;
-    
-            }
-    
-        }
-    
-        elements.projectList.innerHTML = projectListTemplate;
-    
-    }
-
-    renderTasks();
-    renderProjects();
 
 }
 
---
+++ src/modules/ui.js
@@ -0,0 +1,146 @@
+class UI {
+
+    constructor(app, elements) {
+
+        this.app = app;
+        this.elements = elements;
+
+    }
+
+    render() {
+
+        const renderTasks = () => {
+    
+            const checkFilterActive = (filterActive, taskActive) => {
+        
+                if (filterActive === "active" && taskActive === true) {
+        
+                    return true;
+        
+                } else if (filterActive === "completed" && taskActive === false) {
+        
+                    return true;
+        
+                } else if (filterActive === "all") {
+        
+                    return true;
+        
+                } else {
+        
+                    return false;
+                    
+                }
+        
+            }
+        
+            const checkFilterDate = (filterDate, taskTimestamp) => {
+        
+                const CURRENT_DATE = new Date().toISOString().slice(0, 10);
+                const CURRENT_TIMESTAMP = Date.parse(CURRENT_DATE);
+                const TASK_SECONDS = Math.floor(taskTimestamp / 1000);
+                const CURRENT_SECONDS =  Math.floor(CURRENT_TIMESTAMP / 1000);
+                const UPCOMING_SECONDS = 604800;
+        
+                if (TASK_SECONDS === CURRENT_SECONDS && filterDate === "today") {
+        
+                    return true;
+        
+                } else if (TASK_SECONDS < CURRENT_SECONDS + UPCOMING_SECONDS && filterDate === "upcoming") {
+        
+                    return true;
+        
+                } else if (filterDate === "all") {
+        
+                    return true;
+        
+                } else {
+        
+                    return false;
+        
+                }
+        
+            }
+            
+            const projects = this.app.getProjectList();
+            const filterActive = this.elements.inputElements.filterActive.value;
+            const filterDate = this.elements.inputElements.filterDate.value;
+            let taskListTemplate = ``;
+        
+            for (const projectName in projects) {
+        
+                const tasks = projects[projectName].getTaskList();
+        
+                for (const taskTitle in tasks) {
+        
+                    const task = tasks[taskTitle];
+                    const taskProject = task.getProject();
+                    const taskDate = task.getDate();
+                    const taskTimestamp = task.getTimestamp();
+                    const taskActive = task.isActive();
+                    const checkBoxState = taskActive ? "" : "checked";
+                    const taskCompleteClass = taskActive ? "" : "-complete";
+            
+                    const taskTemplate = `
+                    <div class="task${taskCompleteClass}" data-task-title="${taskTitle}" data-task-project="${taskDate}" data-task-active="${taskActive}">
+                        <input type="checkbox" id="complete-${taskTitle}" class="complete-task-checkbox" ${checkBoxState}>
+                        <div class="task-content">
+                            <label for="complete-${taskTitle}" class="task-title">${taskTitle}</label>
+                            <p class="task-date">${taskDate}</p>
+                        </div>
+                        <button type="button" class="remove-task-button">X</button>
+                    </div>
+                    `;
+            
+                    if (this.app.getActiveProject() === taskProject && checkFilterActive(filterActive, taskActive) && checkFilterDate(filterDate, taskTimestamp)) {
+            
+                        taskListTemplate += taskTemplate;
+            
+                    }
+            
+                }
+        
+            }
+        
+            this.elements.taskList.innerHTML = taskListTemplate;
+        
+        }
+        
+        const renderProjects = () => {
+        
+            const projects = this.app.getProjectList();
+            let projectListTemplate = ``;
+        
+            for (const projectName in projects) {
+    
+                const projectActiveClass = this.app.getActiveProject() === projectName ? "-active" : "";
+        
+                if (projectName !== this.app.main.getName()) {
+        
+                    const projectTemplate = `
+                    <span class="project${projectActiveClass}" data-project-name="${projectName}">${projectName}<button class="remove-project-button">X</button></span>
+                    `
+                    projectListTemplate += projectTemplate;
+        
+                } else {
+        
+                    const projectTemplate = `
+                    <span class="project${projectActiveClass}" data-project-name="main">Main</span>
+                    `
+                    projectListTemplate += projectTemplate;
+        
+                }
+        
+            }
+        
+            this.elements.projectList.innerHTML = projectListTemplate;
+        
+        }
+    
+        renderTasks();
+        renderProjects();
+    
+    }
+
+}
+
+export default UI;
